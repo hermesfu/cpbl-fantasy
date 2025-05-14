48b12e60d49c3fd2bd1e3c86f9f6e78e1cb1df6b
hfu314 <fu420@purdue.edu>
2025-05-14 17:19:49 -0400

    set up environment variable access

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+msg = os.getenv("MESSAGE")
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return msg
 
 if __name__ == '__main__':
     app.run(debug=True)
