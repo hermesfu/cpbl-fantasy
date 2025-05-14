385445dab390c15f82b6b498c1bbeb4abd5cf681
hfu314 <fu420@purdue.edu>
2025-05-14 17:32:14 -0400

    connect to database

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from dotenv import load_dotenv
 import os
+from pymongo import MongoClient
 
+# Load environment variables
 load_dotenv()
 msg = os.getenv("MESSAGE")
+mongo_url = os.getenv("MONGO_URI")
+
+# Connect to MongoDB
+client = client = MongoClient(mongo_url)
 
 app = Flask(__name__)
 
